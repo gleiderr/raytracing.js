24dfe0606aec7b6fa1d6648e271e3cc371d10d55
Gleider Ribeiro Costa <gleiderr@gmail.com>
2018-05-02 13:04:29 -0300

    Sphere intersection.

--- gaal.js
+++ gaal.js
@@ -6,6 +6,12 @@ export class Gaal {
 		return [u1, u2, u3];
 	}
 
+	static dotprod(v1, v2) {
+		let p = 0;
+		for(let i = 0; i < v1.length; i++) p += v1[i] * v2[i];
+		return p;
+	}
+
 	static norm(v) {
 		let n = v.reduce((a, b) => a + b*b , 0);
 		//console.log('norm', Math.sqrt(n));
--- index.js
+++ index.js
@@ -9,13 +9,16 @@ class Sphere {
 	}
 
 	intersection(R) {
+		let u = R.u;
+		let w = Gaal.sub(this.c, R.p);
+
 		let a = 1;
-		let b = -2*gaal.dotprod(u, w);
-		let c = gaal.dotprod(w, w) - Math.pow(this.r, 2);
+		let b = -2 * Gaal.dotprod(u, w);
+		let c = Gaal.dotprod(w, w) - Math.pow(this.r, 2);
 
 		let delta = Math.pow(b, 2) - 4 * a * c;
 		
-		if(delta < 0) return { t: 0 };
+		if(delta < 0) return { t: Number.MAX_VALUE };
 		
 		let t_minus = (-b - Math.sqrt(delta))/2;
 		let t_plus  = (-b + Math.sqrt(delta))/2;
@@ -55,7 +58,7 @@ export function rayTrace(width, heigth, setpixel) {
 	let Vvz = Gaal.prod(-1, view);
 	let Vvx = Gaal.crossprod(view, up);
 	let Vvy = Gaal.crossprod(Vvz, Vvx);
-	console.log(Vvz, Vvx, Vvy);
+	//console.log(Vvz, Vvx, Vvy);
 	
 	let p = [], u = [], R = [];
 	for(let r = 0; r < heigth; r++) {
@@ -66,34 +69,51 @@ export function rayTrace(width, heigth, setpixel) {
 			//if(c == 0) debugger;
 			p[r][c] = Gaal.plus(eye, Gaal.prod(ax, Vvx), Gaal.prod(ay, Vvy), Gaal.prod(-1, Vvz));
 			u[r][c] = Gaal.normalize(Gaal.sub(p[r][c], eye));
-			R[r][c] = new Ray(eye, Number.MAX_SAFE_INTEGER, u[r][c]);
+			R[r][c] = new Ray(eye, u[r][c], Number.MAX_VALUE);
 
-			setpixel(c, r, c == r ? {r: 0, g: 0, b: 0} : {r: 255, g: 255, b: 255})
-			//setpixel(c, r, /*rgb*/ trace(R));
+			//setpixel(c, r, c == r ? {r: 0, g: 0, b: 0} : {r: 255, g: 255, b: 255})
+			setpixel(c, r, /*rgb*/ trace(R[r][c]));
 		}
 	}
 }
 
+let objects = [
+	new Sphere([3, 3, 15], 1),
+];
+
 // /* Shoot R into the scene and let X be the first object hit and p be the point of contact with this object. */
 function trace(R) {
 	//let X be the first object hit and p be the point of contact with this object.
-	for(obj in objects) {
-		let {t, normal, object, inside} = intersection();
+	let current_obj = null;
+	let current_t = Number.MAX_VALUE;
+
+	for(let obj of objects) {
+		let { t, inside } = obj.intersection(R);
+		if(t < current_t) {
+			current_t = t;
+			current_obj = obj;
+		}
 	}
 
-	if(X.reflective) {
-		//compute the reflection ray Rr of R at p
-		Cr = trace(Rr);
-	}
-	if(X.transparent) {
-		//compute the transmission (refraction) ray Rt of R at p
-		if(inside) /*...*/ ;
-		Ct = trace(Rt);
+	if(current_t < Number.MAX_VALUE) {
+		return {r: 255, g: 255, b: 255};
+	} else {
+		return {r: 0, g: 0, b: 0};
 	}
 
-	for (light of lights) {
-		Cl += trace(Rl);
-	}
+	// if(X.reflective) {
+	// 	//compute the reflection ray Rr of R at p
+	// 	Cr = trace(Rr);
+	// }
+	// if(X.transparent) {
+	// 	//compute the transmission (refraction) ray Rt of R at p
+	// 	if(inside) /*...*/ ;
+	// 	Ct = trace(Rt);
+	// }
 
-	return Cr + Ct + Cl;
+	// for (light of lights) {
+	// 	Cl += trace(Rl);
+	// }
+
+	//return Cr + Ct + Cl;
 }
